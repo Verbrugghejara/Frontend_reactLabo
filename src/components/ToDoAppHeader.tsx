e430e6341cbb6c27ce61719bbb827bb514702eab
Jara Verbrugghe <jara.verbrugghe@student.howest.be>
2022-03-09 15:25:38 +0100

    feat: more layout changes

--- src/components/ToDoAppHeader.tsx
+++ src/components/ToDoAppHeader.tsx
@@ -1,7 +1,11 @@
+import '../style/components/ToDoHeader.scss'
+
+
 const ToDoAppHeader = ()=>{
 return(
-    <header>
-        <p>header</p>
+    <header className="c-todo-app-header">
+        <h1 className="c-todo-app-header__title">Hello {'Jara'}</h1>
+        <h2 className="c-todo-app-header__subtitle">There are {3} todos.</h2>
     </header>
 )
 }
